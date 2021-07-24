c0fe303df4fd9bfdd33b8122fccfaaa4263cfffe
Ken Aida <aidken+github@gmail.com>
2021-07-24 22:48:11 +0900

    Progress to page 47.

--- ch2/__tests__/password-verifier1.test.js
+++ ch2/__tests__/password-verifier1.test.js
@@ -1,30 +1,50 @@
 /*jshint esversion: 6 */
 // cSpell:ignore esversion
 
-const { verifyPassword1 } = require('../password-verifier1');
+const { verifyPassword1, PasswordVerifier1 } = require('../password-verifier1');
 
 describe('verifyPassword', function() {
 
-    test('given a failing rule, returns errors', function() {
-        const fakeRule = function(input) {
-            return {
-                'passed': false,
-                'reason': 'fake reason'
+    describe('with a failing rule', function() {
+
+        test('has an error message based on rule.reason', function() {
+
+            const verifier = new PasswordVerifier1();
+
+            const failingRule = function(input) {
+                return {
+                    'passed': false,
+                    'reason': 'fake reason'
+                };
             };
-        };
-        const errors = verifyPassword('any rule', [fakeRule]);
-        expect(errors[0]).toContain('fake reason');
-    });
+
+            verifier.addRule(failingRule);
+
+            const errors = verifier.verify('any value');
+
+            expect(errors.length).toBe(1);
+            expect(errors[0]).toContain('fake reason');
+        });
+
+    }); // end describe with a failing rule
 
     test('given a successful rule, returns no errors', function() {
-        const fakeRule = function(input) {
+
+        const verifier = new PasswordVerifier1();
+
+        const successfulRule = function(input) {
             return {
                 'passed': true,
                 'reason': 'always successful'
             };
         };
-        const errors = verifyPassword('any rule', [fakeRule]);
+
+        verifier.addRule(successfulRule);
+
+        const errors = verifier.verify('successful rule');
+
         expect(errors).toEqual([]);
+
     });
 
-});
+}); // end describe verifyPassword
